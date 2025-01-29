2660cf06fc4cc2f9e186b60e2cb07eab8788c852
AVAR MITTAL <70195066+avar-hub@users.noreply.github.com>
2025-01-29 23:41:17 +0530

    Time: 0 ms (100%), Space: 41.7 MB (71.77%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -5,16 +5,17 @@ class Solution {
         for(int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 int cell = board[i][j];
+                int lives = checkLive(i, j, board, directions);
                 if (cell == 0) {
-                    if(checkReproduction(i, j, board, directions)) {
+                    if(lives == 3) {
                         newStage[i][j] = 1;
                     } else {
                         newStage[i][j] = 0;
                     }
                 } else {
-                    if(checkUnderPopulation(i, j, board, directions)) {
+                    if(lives < 2) {
                         newStage[i][j] = 0;
-                    } else if(checkOverPopulation(i, j, board, directions)) {
+                    } else if(lives > 3) {
                         newStage[i][j] = 0;
                     } else {
                         newStage[i][j] = 1;
@@ -29,7 +30,7 @@ class Solution {
     }
     }
 
-    private boolean checkReproduction(int i , int j, int[][] board, int[][] direction) {
+    private int checkLive(int i , int j, int[][] board, int[][] direction) {
         int liveCellCount = 0;
        for ( int[] dir : direction) {
         int newRow = i + dir[0];
@@ -40,47 +41,7 @@ class Solution {
             }
         }
        }
-
-       if(liveCellCount == 3) {
-        return true;
-       } 
-       return false;
-
-    }
-
-    private boolean checkUnderPopulation(int i , int j , int[][] board,  int[][] direction) {
-        int liveCellCount = 0;
-       for ( int[] dir : direction) {
-        int newRow = i + dir[0];
-        int newCol = j + dir[1];
-        if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
-            if(board[newRow][newCol] == 1) {
-                liveCellCount++;
-            }
-        }
-       }
-
-       if(liveCellCount < 2) {
-        return true;
-       } 
-       return false;
-    }
-
-    private boolean checkOverPopulation(int i , int j , int[][] board,  int[][] direction) {
-        int liveCellCount = 0;
-       for ( int[] dir : direction) {
-        int newRow = i + dir[0];
-        int newCol = j + dir[1];
-        if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
-            if(board[newRow][newCol] == 1) {
-                liveCellCount++;
-            }
-        }
-       }
-       if(liveCellCount > 3) {
-        return true;
-       } 
-       return false;
+       return liveCellCount;
     }
 
 }
