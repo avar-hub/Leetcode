f1a34af23ba4063e2e28e1db73d157ee851dc8d3
AVAR MITTAL <70195066+avar-hub@users.noreply.github.com>
2025-02-28 00:24:08 +0530

    Time: 10 ms (60.15%), Space: 45.5 MB (23.34%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,30 +1,18 @@
-import java.util.Arrays;
-
 class Solution {
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, Comparator.comparingInt(a -> a[1])); 
-        int[][] dp = new int[pairs.length][pairs.length + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1); 
-        }
-        return helper(pairs, 0, -1, dp);
-    }
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[1]));
 
-    private int helper(int[][] pairs, int index, int lastPairIndex, int[][] dp) {
-        if (index >= pairs.length) return 0;
+        int[] prev = pairs[0];
+        int res = 1;
 
-        if (dp[index][lastPairIndex + 1] != -1) {
-            return dp[index][lastPairIndex + 1];
+        for (int i = 1; i < pairs.length; i++) {
+            int[] cur = pairs[i];
+            if (cur[0] > prev[1]) {
+                res++;
+                prev = cur;
+            }
         }
 
-        int pick = 0;
-        if (lastPairIndex == -1 || pairs[lastPairIndex][1] < pairs[index][0]) {
-            pick = 1 + helper(pairs, index + 1, index, dp); 
-        }
-        int notPick = helper(pairs, index + 1, lastPairIndex, dp); 
-
-        dp[index][lastPairIndex + 1] = Math.max(pick, notPick); 
-
-        return dp[index][lastPairIndex + 1];
+        return res;        
     }
 }
