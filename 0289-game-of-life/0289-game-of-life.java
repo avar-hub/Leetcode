9bde4d5246258e3b6f20cb23498ec27499b0c489
AVAR MITTAL <70195066+avar-hub@users.noreply.github.com>
2026-03-11 23:50:41 +0530

    Time: 0 ms (100%), Space: 43 MB (89.75%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,47 +1,45 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int[][] newStage = new int[board.length][board[0].length];
-        int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0},  {-1, -1}, {1, 1},  {-1, 1}, {1, -1}  };        
-        for(int i = 0; i < board.length; i++) {
+        int[][] ans = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                int cell = board[i][j];
-                int lives = checkLive(i, j, board, directions);
-                if (cell == 0) {
-                    if(lives == 3) {
-                        newStage[i][j] = 1;
-                    } else {
-                        newStage[i][j] = 0;
-                    }
-                } else {
-                    if(lives < 2) {
-                        newStage[i][j] = 0;
-                    } else if(lives > 3) {
-                        newStage[i][j] = 0;
-                    } else {
-                        newStage[i][j] = 1;
-                    }
-                }
+                if (board[i][j] == 1 && liveNeighbours(i, j, board) < 2) {
+                    ans[i][j] = 0;
+                } else if (board[i][j] == 1 && liveNeighbours(i, j, board) > 3) {
+                    ans[i][j] = 0;
+                } else if (board[i][j] == 0 && liveNeighbours(i, j, board) == 3) {
+                    ans[i][j] = 1;
+                } else if (board[i][j] == 1 && liveNeighbours(i, j, board) >= 2 && liveNeighbours(i, j, board) <= 3) {
+                    ans[i][j] = 1;
+                } else ans[i][j] = 0;
             }
         }
-    for(int i = 0; i < board.length ; i++) {
-        for( int j = 0; j < board[0].length; j++) {
-            board[i][j] = newStage[i][j];
-        }
-    }
-    }
-
-    private int checkLive(int i , int j, int[][] board, int[][] direction) {
-        int liveCellCount = 0;
-       for ( int[] dir : direction) {
-        int newRow = i + dir[0];
-        int newCol = j + dir[1];
-        if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
-            if(board[newRow][newCol] == 1) {
-                liveCellCount++;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = ans[i][j];
             }
         }
-       }
-       return liveCellCount;
     }
 
+    private int liveNeighbours(int i, int j, int[][] board) {
+        int count = 0;
+        if (i > 0 && board[i - 1][j] == 1)
+            count++;
+        if (j > 0 && board[i][j - 1] == 1)
+            count++;
+        if (i < board.length - 1 && board[i + 1][j] == 1)
+            count++;
+        if (j < board[0].length - 1 && board[i][j + 1] == 1)
+            count++;
+        if (i > 0 && j < board[0].length - 1 && board[i - 1][j + 1] == 1)
+            count++;
+        if (j > 0 && i < board.length - 1 && board[i + 1][j - 1] == 1)
+            count++;
+        if (i < board.length - 1 && j < board[0].length - 1 && board[i + 1][j + 1] == 1)
+            count++;
+        if (j > 0 && i > 0 && board[i - 1][j - 1] == 1)
+            count++;
+
+        return count;
+    }
 }
