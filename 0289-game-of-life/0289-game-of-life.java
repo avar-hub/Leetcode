5e07d4f78eb475a435b7f0c2c7b306e901bb6a31
AVAR MITTAL <70195066+avar-hub@users.noreply.github.com>
2026-03-11 23:55:15 +0530

    Time: 0 ms (100%), Space: 43.6 MB (9.38%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,44 +1,49 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int[][] ans = new int[board.length][board[0].length];
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 1 && liveNeighbours(i, j, board) < 2) {
-                    ans[i][j] = 0;
-                } else if (board[i][j] == 1 && liveNeighbours(i, j, board) > 3) {
-                    ans[i][j] = 0;
-                } else if (board[i][j] == 0 && liveNeighbours(i, j, board) == 3) {
-                    ans[i][j] = 1;
-                } else if (board[i][j] == 1 && liveNeighbours(i, j, board) >= 2 && liveNeighbours(i, j, board) <= 3) {
-                    ans[i][j] = 1;
-                } else ans[i][j] = 0;
+
+                int live = liveNeighbours(i, j, board);
+
+                if (board[i][j] == 1 && live < 2) {
+                    board[i][j] = -1;
+                } 
+                else if (board[i][j] == 1 && live > 3) {
+                    board[i][j] = -1;
+                } 
+                else if (board[i][j] == 0 && live == 3) {
+                    board[i][j] = 2;
+                }
             }
         }
+
+        // convert encoded values to final state
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                board[i][j] = ans[i][j];
+
+                if (board[i][j] == -1)
+                    board[i][j] = 0;
+
+                if (board[i][j] == 2)
+                    board[i][j] = 1;
             }
         }
     }
 
     private int liveNeighbours(int i, int j, int[][] board) {
+
         int count = 0;
-        if (i > 0 && board[i - 1][j] == 1)
-            count++;
-        if (j > 0 && board[i][j - 1] == 1)
-            count++;
-        if (i < board.length - 1 && board[i + 1][j] == 1)
-            count++;
-        if (j < board[0].length - 1 && board[i][j + 1] == 1)
-            count++;
-        if (i > 0 && j < board[0].length - 1 && board[i - 1][j + 1] == 1)
-            count++;
-        if (j > 0 && i < board.length - 1 && board[i + 1][j - 1] == 1)
-            count++;
-        if (i < board.length - 1 && j < board[0].length - 1 && board[i + 1][j + 1] == 1)
-            count++;
-        if (j > 0 && i > 0 && board[i - 1][j - 1] == 1)
-            count++;
+
+        if (i > 0 && Math.abs(board[i - 1][j]) == 1) count++;
+        if (j > 0 && Math.abs(board[i][j - 1]) == 1) count++;
+        if (i < board.length - 1 && Math.abs(board[i + 1][j]) == 1) count++;
+        if (j < board[0].length - 1 && Math.abs(board[i][j + 1]) == 1) count++;
+
+        if (i > 0 && j < board[0].length - 1 && Math.abs(board[i - 1][j + 1]) == 1) count++;
+        if (j > 0 && i < board.length - 1 && Math.abs(board[i + 1][j - 1]) == 1) count++;
+        if (i < board.length - 1 && j < board[0].length - 1 && Math.abs(board[i + 1][j + 1]) == 1) count++;
+        if (j > 0 && i > 0 && Math.abs(board[i - 1][j - 1]) == 1) count++;
 
         return count;
     }
